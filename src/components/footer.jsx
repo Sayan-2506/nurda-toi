88a8112b92309945363f4c8bf0bdafe407694745
Sayan-2506 <urustemovsayan25@gmail.com>
2023-08-03 15:43:16 +0600

    change number

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,10 +5,10 @@ import instaImg from "../assets/insta.png";
 const Footer = () => {
   return (
     <div className="m-auto mt-20 px-8 py-3 w-full max-w-[600px] h-26 bg-slate-400 flex flex-col text-center text-slate-700">
-      <div className="text-xs font-medium text-gray-50">@made by <span className="underline decoration-indigo-500">toiga_sait_shakirtu</span></div>
+      <div className="text-xs font-medium text-gray-50">@made by toiga_sait_shakirtu</div>
       <div className="flex flex-wrap justify-center items-center mt-2 gap-1">
         <a
-          href="https://wa.me/87718544450"
+          href="https://wa.me/+77718544450"
           target="_blank"
           without
           rel="noreferrer"
